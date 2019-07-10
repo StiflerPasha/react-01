81ae2d1366508374cc3e4f66f87cf2150ee61a47
StiflerPasha <dvoynospavel@gmail.com>
2019-07-10 18:42:03 +0300

    visual changes

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,48 +1,50 @@
-import React from "react";
-import classes from "./Dialogs.module.css";
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Message/Message"
-import photo from "../../assets/images/ava.jpg"
+import React      from 'react';
+import classes    from './Dialogs.module.css';
+import DialogItem from './DialogItem/DialogItem';
+import Message    from './Message/Message';
+import photo      from '../../assets/images/ava.jpg';
 
 const Dialogs = (props) => {
 
-	 const dialogsElements = props.friends
-		 .map(d => <DialogItem key={d.id} id={d.id} name={d.name} ava={d.photos.small || photo}/>);
+   const dialogsElements = props.friends.map(d =>
+	  <DialogItem key={ d.id } id={ d.id } name={ d.name } ava={ d.photos.small || photo } />);
 
-	 const messagesElements = props.messages
-		 .map(m => <Message key={m.id} msg={m.msg}/>);
+   const messagesElements = props.messages.map(m =>
+	  <Message key={ m.id } msg={ m.msg } />);
 
-	 const onSendMessageClick = () => {
-			props.sendMessage();
-	 };
+   const onSendMessageClick = () => {
+	  props.sendMessage();
+   };
 
-	 const onNewMessageChange = (event) => {
-			let body = event.currentTarget.value;
-			props.updateNewMessageBody(body);
-	 };
+   const onNewMessageChange = (event) => {
+	  let body = event.currentTarget.value;
+	  props.updateNewMessageBody(body);
+   };
 
-	 return (
-		 <div className={classes.dialogs}>
-				<div>
-					 {dialogsElements}
-				</div>
-				<div>
-					 <div>{messagesElements}</div>
-					 <div>
-							<div>
-								 <textarea
-									 onChange={onNewMessageChange}
-									 placeholder="Enter your message"
-									 value={props.newMessagesBody}>
-								 </textarea>
-							</div>
-							<div>
-								 <button onClick={onSendMessageClick}>Send</button>
-							</div>
-					 </div>
-				</div>
+   return (
+	  <div className={ classes.dialogs }>
+		 <div>
+			{ dialogsElements }
 		 </div>
-	 );
+		 <div>
+			<div>
+			   { messagesElements }
+			</div>
+			<div>
+			   <div>
+				  <textarea
+					 onChange={ onNewMessageChange }
+					 placeholder="Enter your message"
+					 value={ props.newMessagesBody }>
+				  </textarea>
+			   </div>
+			   <div>
+				  <button onClick={ onSendMessageClick }>Send</button>
+			   </div>
+			</div>
+		 </div>
+	  </div>
+   );
 };
 
 export default Dialogs;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import classes from './Users.module.css'
-import photo from '../../assets/images/ava_post.jfif'
-import {NavLink} from "react-router-dom";
-import {useSelector} from 'react-redux'
+import React           from 'react';
+import classes         from './Users.module.css';
+import photo           from '../../assets/images/ava_post.jfif';
+import { NavLink }     from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 const Users = (props) => {
-	 let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-	 let pages = [];
-	 for (let i = 1; i <= pagesCount; i++) pages.push(i);
+   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+   let pages = [];
+   for (let i = 1; i <= pagesCount; i++) pages.push(i);
 
 
-	 const pagination = pages.map((page, i) => (
-		 <span key={i}
-					 className={classes.pageCount + " " + (props.currentPage === page ? classes.activePageCount : "")}
-					 onClick={() => props.onPageCountChanged(page)}>
-				{`${page} `}
+   const pagination = pages.map((page, i) => (
+	  <span key={ i }
+			className={ classes.pageCount + ' ' + (props.currentPage === page ? classes.activePageCount : '') }
+			onClick={ () => props.onPageCountChanged(page) }>
+			{ `${ page } ` }
 		 </span>
-	 ));
+   ));
 
-	 const users = props.users.map(u =>
-		 <div key={u.id}>
-				<div>
-					 <NavLink to={'/profile/' + u.id}>
-							<img src={u.photos.small || photo}
-									 className={classes.userPhoto}
-									 alt='User-Ava'/>
-					 </NavLink>
-					 <div>
-							{u.followed ?
-								<button onClick={() => props.describe(u.id)}
-								>Describe
-								</button> :
-								<button onClick={() => props.follow(u.id)}
-								>Follow
-								</button>}
-					 </div>
-				</div>
+   const users = props.users.map(u =>
+	  <div key={ u.id }>
+		 <div className={ classes.user }>
+			<NavLink to={ '/profile/' + u.id }>
+			   <img src={ u.photos.small || photo }
+					className={ classes.userPhoto }
+					alt="User-Ava" />
+			</NavLink>
+			<div className={ classes.userName }>Name: { u.name }</div>
+			<div>Status: { u.status || 'Offline' }</div>
+			<div className={ classes.button_follow }>
+			   { u.followed ?
+				  <button onClick={ () => props.describe(u.id) }
+				  >Describe
+				  </button> :
+				  <button onClick={ () => props.follow(u.id) }
+				  >Follow
+				  </button> }
+			</div>
+		 </div>
 
-				<div className={classes.userName}>Name: {u.name}</div>
-				<div>Status: {u.status || "Offline"}</div>
-				{/*<div>Birthday: {new Date(u.dob.date).toLocaleDateString()}</div>
+		 {/*<div>Birthday: {new Date(u.dob.date).toLocaleDateString()}</div>
 				<div>Country: {u.nat.toLocaleString()}</div>
 				<div className={classes.userCity}>
 					 City:
@@ -48,20 +48,21 @@ const Users = (props) => {
 							target='_blank' rel="noopener noreferrer">
 							{u.location.city}
 					 </a>
-				</div>*/}
-		 </div>
-	 );
+				</div>*/ }
+	  </div>,
+   );
 
 
-	 const usersCount = useSelector(state => state.usersPage.totalUsersCount);
+   const usersCount = useSelector(state => state.usersPage.totalUsersCount);
 
-	 return (
-		 <>
-				<div>{usersCount}</div>
-				{pagination}
-				{props.isLoading ? <div>Loading</div> : users}
-		 </>
-	 )
+   return (
+	  <>
+		 <div>Total users count: { usersCount }</div>
+		 <hr />
+		 { pagination }
+		 { props.isLoading ? <div>Loading</div> : users }
+	  </>
+   );
 };
 
-export default Users
+export default Users;
