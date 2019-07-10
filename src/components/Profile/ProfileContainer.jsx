2cf8dc5f487bcc2aa9184b367bb43f3eb34a82db
StiflerPasha <dvoynospavel@gmail.com>
2019-07-10 21:38:04 +0300

    update dependencies

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,50 +1,50 @@
-import React from 'react';
-import classes from './Profile.module.css'
-import UserAvatar from './UserAvatar/UserAvatar';
-import MyPostsContainer from "./MyPosts/MyPostsContainer";
-import UserInfo from "./UserInfo/UserInfo";
-import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
-import {getProfile, setUserProfile} from "../../redux/profile_reducer";
+import React                          from 'react';
+import classes                        from './Profile.module.css';
+import UserAvatar                     from './UserAvatar/UserAvatar';
+import MyPostsContainer               from './MyPosts/MyPostsContainer';
+import UserInfo                       from './UserInfo/UserInfo';
+import { connect }                    from 'react-redux';
+import { withRouter }                 from 'react-router-dom';
+import { getProfile, setUserProfile } from '../../redux/profile_reducer';
 
-import ava from '../../assets/images/ava.jpg'
+import ava from '../../assets/images/ava.jpg';
 
 
 class ProfileContainer extends React.Component {
 
-	 componentDidMount() {
-			let id = this.props.match.params.id || this.props.defaultId;
-			this.props.getProfile(id)
-	 }
+   componentDidMount() {
+	  let id = this.props.match.params.id || this.props.defaultId;
+	  this.props.getProfile(id);
+   }
 
-	 componentWillReceiveProps(nextProps, nextContext) {
-			if (nextProps.match.params.id !== this.props.match.params.id) {
-				 let id = nextProps.match.params.id;
-				 this.props.getProfile(id)
-			}
-	 }
+   componentWillReceiveProps(nextProps, nextContext) {
+	  if (nextProps.match.params.id !== this.props.match.params.id) {
+		 let id = nextProps.match.params.id;
+		 this.props.getProfile(id);
+	  }
+   }
 
-	 render() {
-			let {fullName, photos} = this.props.profile;
+   render() {
+	  let { fullName, photos } = this.props.profile;
 
 
-			return this.props.isLoading
-				? (<div>Loading</div>)
-				: (<div className={classes.profile_page}>
-					 <UserAvatar className={classes.info}
-											 ava={photos.large || ava}/>
-					 <UserInfo className={classes.data}
-										 name={fullName}/>
-					 <MyPostsContainer
-						 className={classes.posts}/>
-				</div>)
-	 }
+	  return this.props.isLoading
+		 ? (<div>Loading</div>)
+		 : (<div className={ classes.profile_page }>
+			<UserAvatar className={ classes.info }
+						ava={ photos.large || ava } />
+			<UserInfo className={ classes.data }
+					  name={ fullName } />
+			<MyPostsContainer
+			   className={ classes.posts } />
+		 </div>);
+   }
 }
 
 const mapStateToProps = (state) => ({
-	 profile: state.profilePage.profile,
-	 isLoading: state.profilePage.isLoading
+   profile: state.profilePage.profile,
+   isLoading: state.profilePage.isLoading,
 });
 
 let WithUrlDataContainerComponent = withRouter(ProfileContainer);
-export default connect(mapStateToProps, {setUserProfile, getProfile})(WithUrlDataContainerComponent)
+export default connect(mapStateToProps, { setUserProfile, getProfile })(WithUrlDataContainerComponent);
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,38 +1,38 @@
-import {connect} from "react-redux";
-import Users from "./Users";
-import {follow, describe, setCurrentPage, getUsers} from "../../redux/users_reducer";
-import React from "react";
+import { connect }                                    from 'react-redux';
+import Users                                          from './Users';
+import { follow, describe, setCurrentPage, getUsers } from '../../redux/users_reducer';
+import React                                          from 'react';
 
 
 class UsersContainer extends React.Component {
 
-	 componentDidMount() {
-			let {currentPage, pageSize} = this.props;
-			this.props.getUsers(currentPage, pageSize)
-	 }
+   componentDidMount() {
+	  let { currentPage, pageSize } = this.props;
+	  this.props.getUsers(currentPage, pageSize);
+   }
 
-	 onPageCountChanged = (pageNumber) => {
-			let {pageSize} = this.props;
-			this.props.setCurrentPage(pageNumber);
-			this.props.getUsers(pageNumber, pageSize)
-	 };
+   onPageCountChanged = (pageNumber) => {
+	  let { pageSize } = this.props;
+	  this.props.setCurrentPage(pageNumber);
+	  this.props.getUsers(pageNumber, pageSize);
+   };
 
-	 render() {
-			return (
-				<Users onPageCountChanged={this.onPageCountChanged} {...this.props}/>
-			)
-	 }
+   render() {
+	  return (
+		 <Users onPageCountChanged={ this.onPageCountChanged } { ...this.props } />
+	  );
+   }
 }
 
 const mapStateToProps = (state) => {
-	 return {
-			users: state.usersPage.users,
-			pageSize: state.usersPage.pageSize,
-			totalUsersCount: state.usersPage.totalUsersCount,
-			currentPage: state.usersPage.currentPage,
-			isLoading: state.usersPage.isLoading
-	 }
+   return {
+	  users: state.usersPage.users,
+	  pageSize: state.usersPage.pageSize,
+	  totalUsersCount: state.usersPage.totalUsersCount,
+	  currentPage: state.usersPage.currentPage,
+	  isLoading: state.usersPage.isLoading,
+   };
 };
 
 export default connect(mapStateToProps,
-	{follow, describe, setCurrentPage, getUsers})(UsersContainer);
+   { follow, describe, setCurrentPage, getUsers })(UsersContainer);
